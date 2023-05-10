c4da608c60995ea74578367fee1d64e35ff245df
LuisaInsua <123460380+LuisaInsua@users.noreply.github.com>
2023-05-10 20:55:55 +0200

    Delete voteModel.js

--- models/voteModel.js
+++
@@ -1,8 +0,0 @@
-const { getConnection } = require("../db/db");
-const voteModel = {};
-voteModel.create = async(userId, linkId) => {
-  const connection = await getConnection();
-  const sql = `INSERT INTO votes (userId, linkId) VALUES (?, ?)`;
-  await connection.query(sql, [userId, linkId]);
-};
-module.exports = voteModel;
