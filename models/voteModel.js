60722fe9c9a2a5502958e3d07a98b35748044d0f
LuisaInsua <lisiinsua@gmail.com>
2023-05-11 20:04:41 +0200

    proyecto actualizado

--- models/voteModel.js
+++ models/voteModel.js
@@ -1,8 +1,19 @@
 const { getConnection } = require("../db/db");
 const voteModel = {};
-voteModel.create = async(userId, linkId) => {
+voteModel.create = async (linkId, userId) => {
   const connection = await getConnection();
   const sql = `INSERT INTO votes (userId, linkId) VALUES (?, ?)`;
   await connection.query(sql, [userId, linkId]);
+};
+voteModel.findByLinkIdAndUserId = async (linkId, userId) => {
+  const connection = await getConnection();
+  const sql = `SELECT * FROM votes WHERE linkId = ? AND userId = ?`;
+  const [rows] = await connection.query(sql, [linkId, userId]);
+  return rows.length ? rows[0] : null;
+};
+voteModel.delete = async (voteId) => {
+  const connection = await getConnection();
+  const sql = `DELETE FROM votes WHERE id = ?`;
+  await connection.query(sql, [voteId]);
 };
 module.exports = voteModel;
