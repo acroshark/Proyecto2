f7b117cc7048c349709f5593c7b52cc5dbf887e4
xoanfa <xoan.fernandez.agrelo@hotmail.com>
2023-06-16 16:56:08 +0200

    actualizacion

--- controllers/routes.js
+++ controllers/routes.js
@@ -5,46 +5,44 @@ const linkModel = require('../models/linkModel');
 const voteModel = require('../models/voteModel');
 const authMiddleware = require('../middleware/auth');
 const routes = (app) => {
-  //Registro de nuevo usuario
-  app.post('/register', async (req, res) => {
-    const name = req.body.name;
-    const email = req.body.email;
-    const password = req.body.password;
-    console.log(password, name, email);
-    console.log(req.body);
-    // Validar longitud y complejidad de contraseña
-    if (
-      password.length > 10 ||
-      !/[A-Z]/.test(password) ||
-      !/[0-9]/.test(password)
-    ) {
-      return res.status(400).json({
-        message:
-          'Password must be 10 characters or less and contain at least one uppercase letter and one number',
-      });
-    }
-    let [result] = await userModel.findByEmail(email);
-    console.log(result);
-    if (result.length > 0) {
-      return res.status(409).json({ message: 'User already exists' });
-    } else {
-      const hash = await bcrypt.hash(password, 10);
-      let userResult = await userModel.create(name, email, hash);
-      console.log(userResult);
-      const user = {
-        id: userResult.insertId,
-        name: name,
-        email: email,
-      };
-      const token = jwt.sign({ user: user }, process.env.JWT_SECRET, {
-        expiresIn: '1h',
-      });
-      res.status(200).json({ token: token });
-      console.log('test2');
+  // Registro de nuevo usuario
+  app.post('/register', async (req, res, next) => {
+    try {
+      const { name, email, password } = req.body;
+      // Validar longitud y complejidad de contraseña
+      if (
+        password.length > 10 ||
+        !/[A-Z]/.test(password) ||
+        !/[0-9]/.test(password)
+      ) {
+        return res.status(400).json({
+          message:
+            'Password must be 10 characters or less and contain at least one uppercase letter and one number',
+        });
+      }
+      let [result] = await userModel.findByEmail(email);
+      if (result.length > 0) {
+        return res.status(409).json({ message: 'User already exists' });
+      } else {
+        const hash = await bcrypt.hash(password, 10);
+        let userResult = await userModel.create(name, email, hash);
+        const user = {
+          id: userResult.insertId,
+          name: name,
+          email: email,
+        };
+        /*const token = jwt.sign({ user }, process.env.JWT_SECRET, {
+          expiresIn: "30d",
+        });*/
+        res.status(200).json({ user });
+      }
+    } catch (error) {
+      res.status(500).json({ message: 'Internal server error' });
+      next(error);
     }
   });
   // Login de usuarios
-  app.post('/login', async (req, res) => {
+  app.post('/login', async (req, res, next) => {
     try {
       const email = req.body.email;
       const password = req.body.password;
@@ -58,56 +56,70 @@ const routes = (app) => {
           const token = jwt.sign({ user: user }, process.env.JWT_SECRET, {
             expiresIn: '30d',
           });
-          res.status(200).json({ token: token });
+          res.status(200).json({ user, token });
         } else {
           res.status(401).json({ message: 'Invalid email or password' });
         }
       }
     } catch (error) {
-      console.error(error);
       res.status(500).json({ message: 'Internal server error' });
+      next(error);
+    }
+  });
+  // Cerrar sesión
+  app.post('/logout', authMiddleware, async (req, res, next) => {
+    try {
+      // Eliminar el token de autenticación del usuario
+      const user = req.user;
+      user.token = null;
+      await userModel.updateToken(user.id, null);
+      res.status(200).json({ message: 'Logout successful' });
+    } catch (error) {
+      res.status(500).json({ message: 'Error logging out' });
+      next(error);
     }
   });
   // obtener todos los enlaces publicados hoy y en días anteriores
-  app.get('/links', authMiddleware, async (req, res) => {
-    console.log(req.query);
-    const { date } = req.query;
-    if (!date) {
-      res.status(400).json({ message: 'date required' });
-    }
+  app.get('/links', authMiddleware, async (req, res, next) => {
     try {
+      console.log(req.query);
+      const { date } = req.query;
+      if (!date) {
+        return res.status(400).json({ message: 'date required' });
+      }
       const result = await linkModel.find(date);
       res.status(200).json(result);
     } catch (err) {
-      console.error(err);
       res.status(500).json({ message: 'Error retrieving links' });
+      next(err);
     }
   });
-  //Crear un nuevo link
-  app.post('/links', authMiddleware, async (req, res) => {
-    console.log('links');
-    const title = req.body.title;
-    const url = req.body.url;
-    const description = req.body.description;
-    const user = req.user;
-    console.log(req.body);
-    if (!user) {
-      res.status(401).json({ message: 'You must be logged in to do that' });
-    } else {
-      try {
-        const result = await linkModel.create(user.id, url, title, description);
-        console.log(result);
-        res
-          .status(200)
-          .json({ message: 'Creado correctamente', id: result.insertId });
-      } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Internal server error' });
+  // Crear un nuevo link
+  app.post('/links', authMiddleware, async (req, res, next) => {
+    try {
+      console.log('links');
+      const title = req.body.title;
+      const url = req.body.url;
+      const description = req.body.description;
+      const user = req.user;
+      console.log(req.body);
+      if (!user) {
+        return res
+          .status(401)
+          .json({ message: 'You must be logged in to do that' });
       }
+      const result = await linkModel.create(user.id, url, title, description);
+      console.log(result);
+      res
+        .status(200)
+        .json({ message: 'Creado correctamente', id: result.insertId });
+    } catch (err) {
+      res.status(500).json({ message: 'Internal server error' });
+      next(err);
     }
   });
   // Borrar un enlace publicado por el usuario
-  app.delete('/links/:id', authMiddleware, async (req, res) => {
+  app.delete('/links/:id', authMiddleware, async (req, res, next) => {
     try {
       const linkId = req.params.id;
       const user = req.user;
@@ -122,43 +134,51 @@ const routes = (app) => {
           .status(403)
           .json({ message: 'You are not authorized to perform this action' });
       }
+      // Eliminar los votos asociados al enlace
+      await voteModel.deleteByLinkId(linkId);
+      // Eliminar el enlace
       await linkModel.delete(linkId);
       res.status(200).json({ message: 'Link deleted successfully' });
     } catch (err) {
-      console.error(err);
       res.status(500).json({ message: 'Internal server error' });
+      next(err);
     }
   });
   // Votar por un link
-  app.post('/links/:id/vote', authMiddleware, async (req, res) => {
-    const linkId = req.params.id;
-    const user = req.user;
-    if (!user) {
-      res.status(401).json({ message: 'You must be logged in to do that' });
-    } else {
-      try {
-        // Verificar si el usuario ya votó para este enlace
-        const existingVote = await voteModel.findByLinkIdAndUserId(
-          linkId,
-          user.id
-        );
-        if (existingVote) {
-          // Eliminar el voto existente
-          await voteModel.delete(existingVote.id);
-          res.status(200).json({ message: 'Vote removed successfully' });
-        } else {
-          // Crear un nuevo voto
-          await voteModel.create(linkId, user.id);
-          res.status(200).json({ message: 'Vote added successfully' });
-        }
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error voting for link' });
+  app.post('/links/:id/vote', authMiddleware, async (req, res, next) => {
+    try {
+      const linkId = req.params.id;
+      const user = req.user;
+      if (!user) {
+        return res
+          .status(401)
+          .json({ message: 'You must be logged in to do that' });
       }
+      // Verificar si el usuario ya votó para este enlace
+      const existingVote = await voteModel.findByLinkIdAndUserId(
+        linkId,
+        user.id
+      );
+      if (existingVote) {
+        // Eliminar el voto existente
+        await voteModel.delete(existingVote.id, user.id);
+        // Actualizar el recuento de votos
+        await voteModel.updateVoteCount(linkId);
+        res.status(200).json({ message: 'Vote removed successfully' });
+      } else {
+        // Crear un nuevo voto
+        await voteModel.create(linkId, user.id);
+        // Actualizar el recuento de votos
+        await voteModel.updateVoteCount(linkId);
+        res.status(200).json({ message: 'Vote added successfully' });
+      }
+    } catch (error) {
+      res.status(500).json({ message: 'Error voting for link' });
+      next(error);
     }
   });
-  //Editar perfil usuario
-  app.put('/users', authMiddleware, async (req, res) => {
+  // Editar perfil de usuario
+  app.put('/users', authMiddleware, async (req, res, next) => {
     try {
       const userId = req.user.id;
       const name = req.body.name;
@@ -183,9 +203,40 @@ const routes = (app) => {
       await userModel.update(userId, name, email, password);
       res.status(200).json({ message: 'User updated successfully' });
     } catch (error) {
-      console.error(error);
       res.status(500).json({ message: 'Server error' });
+      next(error);
     }
+  });
+  // Ruta para eliminar un usuario
+  app.delete('/users/:id', authMiddleware, async (req, res, next) => {
+    try {
+      const userId = req.params.id;
+      const user = req.user;
+      if (user.id !== parseInt(userId)) {
+        return res
+          .status(403)
+          .json({ message: 'You are not authorized to perform this action' });
+      }
+      // Eliminar los enlaces asociados al usuario
+      await linkModel.deleteByUserId(userId);
+      // Eliminar el usuario
+      await userModel.deleteById(userId);
+      res.status(200).json({ message: 'User deleted successfully' });
+    } catch (err) {
+      res.status(500).json({ message: 'Internal server error' });
+      next(err);
+    }
+  });
+  //Middleware de error  (4 parametros empezando por error)
+  app.use((err, req, res, next) => {
+    console.log('ERROR: ' + err.message);
+    // res.status(500);
+    res.send('Algo salió mal');
+  });
+  //Middleware 404 (va al final y no tiene next)
+  app.use((req, res, next) => {
+    res.status(404);
+    res.send('Página no encontrada');
   });
 };
 module.exports = routes;
