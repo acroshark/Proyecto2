6d72124ca896b43c806c4bc831978fde3e34bf2a
LuisaInsua <lisiinsua@gmail.com>
2023-06-17 08:22:36 +0200

    actualizado

---
+++ backend/middleware/auth.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const userModel = require("../users/userModel");
+const authenticateUser = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    console.log(token);
+    let tokenData;
+    try {
+      tokenData = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (e) {
+      throw new Error("Token incorrecto");
+    }
+    const { user } = tokenData;
+    console.log(user);
+    let [userDB] = await userModel.findById(user.id);
+    if (!userDB) {
+      return res.sendStatus(403);
+    }
+    req.user = userDB[0];
+    next();
+  } else {
+    res.status(401).json({ message: "You must be logged in to do that" });
+  }
+};
+module.exports = authenticateUser;
---
+++ backend/models/linkModel.js
@@ -0,0 +1,39 @@
+const { getConnection } = require("../db/db");
+const linkModel = {};
+linkModel.create = async (userId, url, title, description) => {
+  console.log(userId, url, title, description);
+  const connection = await getConnection();
+  const sql = `INSERT INTO links (userId, url, title, description) VALUES (?, ?, ?, ?)`;
+  let [result] = await connection.query(sql, [userId, url, title, description]);
+  connection.release();
+  return result;
+};
+linkModel.findById = async (id) => {
+  const connection = await getConnection();
+  const sql = `SELECT * FROM links WHERE id = ?`;
+  let [result] = await connection.query(sql, [id]);
+  connection.release();
+  return result;
+};
+linkModel.find = async (date) => {
+  const connection = await getConnection();
+  const sql = `SELECT links.id, links.userId, links.url, links.title, links.description, links.createdAt, count(v.id) as votes FROM links left join votes v on links.id = v.linkId  WHERE convert(links.createdAt,date) = ? group by links.id`;
+  let [result] = await connection.query(sql, [date]);
+  connection.release();
+  return result;
+};
+linkModel.delete = async (id) => {
+  const connection = await getConnection();
+  const sql = `DELETE  FROM links WHERE id = ? `;
+  let result = await connection.query(sql, [id]);
+  connection.release();
+  return result;
+};
+linkModel.deleteByUserId = async (userId) => {
+  const connection = await getConnection();
+  const sql = `DELETE FROM links WHERE userId = ?`;
+  let result = await connection.query(sql, [userId]);
+  connection.release();
+  return result;
+};
+module.exports = linkModel;
