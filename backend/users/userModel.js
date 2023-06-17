3484d2b15d0bbb4283a0bc72f93ae315d1909c0e
LuisaInsua <lisiinsua@gmail.com>
2023-06-17 07:52:14 +0200

    actualizado

---
+++ backend/users/userModel.js
@@ -0,0 +1,59 @@
+const { getConnection } = require("../db/db");
+const userModel = {};
+userModel.create = async (name, email, password) => {
+  const connection = await getConnection();
+  const sql = `INSERT INTO users (name, email, password) VALUES (?, ?, ?)`;
+  let result = await connection.query(sql, [name, email, password]);
+  connection.release();
+  return result;
+};
+userModel.findByEmail = async (email) => {
+  const connection = await getConnection();
+  const sql = `SELECT * FROM users WHERE email = ?`;
+  let result = await connection.query(sql, [email]);
+  connection.release();
+  return result;
+};
+userModel.findById = async (id) => {
+  const connection = await getConnection();
+  const sql = `SELECT * FROM users WHERE id = ?`;
+  let result = await connection.query(sql, [id]);
+  connection.release();
+  return result;
+};
+userModel.update = async (id, name, email, password) => {
+  const connection = await getConnection();
+  const sql = `UPDATE users SET name=?, email=?, password=? WHERE id=?`;
+  let result = await connection.query(sql, [name, email, password, id]);
+  connection.release();
+  return result;
+};
+userModel.delete = async (id) => {
+  const connection = await getConnection();
+  const sql = `DELETE FROM users WHERE id = ?`;
+  let result = await connection.query(sql, [id]);
+  connection.release();
+  return result;
+};
+userModel.deleteById = async (id) => {
+  const connection = await getConnection();
+  const sql = `DELETE FROM users WHERE id = ?`;
+  let result = await connection.query(sql, [id]);
+  connection.release();
+  return result;
+};
+userModel.deleteByUserId = async (userId) => {
+  const connection = await getConnection();
+  const sql = `DELETE FROM links WHERE userId = ?`;
+  let result = await connection.query(sql, [userId]);
+  connection.release();
+  return result;
+};
+userModel.updateToken = async (id, token) => {
+  const connection = await getConnection();
+  const sql = `UPDATE users SET token=? WHERE id=?`;
+  let result = await connection.query(sql, [token, id]);
+  connection.release();
+  return result;
+};
+module.exports = userModel;
